080d50a90a7cec608587ae29be1c555b4fba6bdf
adam tarango <you@example.comadam.tarango97@gmail.comgit commitgit statusgit statusgit config --global user.email you@example.comadam.tarango97@gmail.com>
2024-12-04 14:32:31 -0800

    add auth router, extend index.js for login/registration, plug into generic server.js

--- backend/prisma/index.js
+++ backend/prisma/index.js
@@ -1,3 +1,26 @@
+const bcrypt = require("bcrypt");
+
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+  model: {
+    user: {
+      async register (username, password) {
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = await prisma.user.create({
+          data : { username, password: hashedPassword }
+        })
+        return user
+      },
+      async login (username, password) {
+        const user = await prisma.user.findUniqueOrThrow({
+          where : { username }
+        });
+        const valid = await bcrypt.compare(password, user.password)
+        if (!valid) throw Error("Invalid password");
+        return user;
+
+      }}    
+  },
+});
+
 module.exports = prisma;
