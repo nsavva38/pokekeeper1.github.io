f070d881d25326b5bf278d4c70f670da15f0eacc
adam tarango <you@example.comadam.tarango97@gmail.comgit commitgit statusgit statusgit config --global user.email you@example.comadam.tarango97@gmail.com>
2024-12-04 14:39:00 -0800

    add .http for testing

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ app.use(require("morgan")("dev"));
 app.use(express.json());
 
 app.use(require("./api/auth").router);
-app.use("/products", require("./api/products"));
-app.use("/orders", require("./api/orders"));
+// app.use("/teams", require("./api/products"));
+// app.use("/pokemon", require("./api/orders"));
 
 app.use((req, res, next) => {
   next({ status: 404, message: "Endpoint not found." });
