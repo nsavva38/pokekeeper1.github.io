c530c90b6144fb0c3947d4e58e2581bd66d90b38
Whuss88 <williamhussey95@gmail.com>
2024-12-12 20:17:50 -0500

    fixed page refreshing w team name w localstate

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const baseURL = 'https://pokekeeper.onrender.com'; 
+const baseURL = 'https://pokekeeper.onrender.com'; // Your deployed backend URL
 
 const api = axios.create({
   baseURL,
   headers: {
-    'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    'Authorization': `Bearer ${localStorage.getItem('token')}`, // Ensure the token is being set correctly
   },
 });
 
