edaf24e2e1a2dace06e2acb34b279a31e86481f2
Nick Savva <nsavva38@gmail.com>
2025-08-12 10:48:39 -0400

    added .env and changed variable to dynamic value

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://pokekeeper1-github-io.onrender.com';
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 const api = axios.create({
   baseURL,
 });
